3301bae699bb416ee5d48e787236fec7a8caf7f2
Toshihiko Nakanishi <binarhythm_bb@yahoo.co.jp>
2021-06-12 13:57:51 +0900

    Initial Commit

---
+++ SwiftUi-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi-Weather
+//
+//  Created by Nakanishi Toshihiko on 2021/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
